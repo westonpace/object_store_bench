da88ebfd9a7754142788c272d0f20edac1f84115
Weston Pace <weston.pace@gmail.com>
2024-08-17 07:40:47 -0700

    WIP

--- src/random_access.rs
+++ src/random_access.rs
@@ -152,14 +152,16 @@ async fn main() {
 
         let total_elapsed = total_start.elapsed();
         let iops_per_second = (takes_per_iter as f64) / total_elapsed.as_secs_f64();
+        let avg_latency = 1.0 / iops_per_second;
         let gibps = (takes_per_iter as f64 * bytes_per_row as f64)
             / total_elapsed.as_secs_f64()
             / (1024.0 * 1024.0 * 1024.0);
         println!(
-            "Total download took {:?} seconds ({} iops/s {} GiB/s)",
+            "Total download took {:?} seconds ({} iops/s {} GiB/s {}s avg latency)",
             total_elapsed.as_secs_f64(),
             iops_per_second,
             gibps,
+            avg_latency,
         );
     }
 }
