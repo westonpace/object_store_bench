e3469d7729ae6f813e0499360185ed3677444d4d
Weston Pace <weston.pace@gmail.com>
2024-08-27 09:28:28 -0700

    WIP

--- src/upload.rs
+++ src/upload.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use clap::Parser;
+use futures::StreamExt;
 use object_store::{gcp::GoogleCloudStorageBuilder, path::Path, ObjectStore, PutPayload};
 
 #[derive(Parser)]
@@ -11,6 +12,9 @@ struct Args {
 
     #[arg(short, long)]
     total_size: Option<u64>,
+
+    #[arg(short, long)]
+    max_parallelism: Option<u64>,
 
     #[arg(short, long)]
     bucket: String,
@@ -30,6 +34,7 @@ async fn main() {
 
     let total_size = args.total_size.unwrap_or(2 * 1024 * 1024 * 1024 * 1024);
     let part_size = args.part_size.unwrap_or(10 * 1024 * 1024);
+    let max_parallelism = args.max_parallelism.unwrap_or(32);
 
     let make_store = move || {
         let builder = GoogleCloudStorageBuilder::new().with_bucket_name(args.bucket.clone());
@@ -49,22 +54,36 @@ async fn main() {
     let mut data = bytes::BytesMut::with_capacity(part_size as usize);
     unsafe { data.set_len(part_size as usize) };
     let data = data.freeze();
-    let mut multipart = store.put_multipart(&path).await.unwrap();
+
+    let multipart = Arc::new(Mutex::new(store.put_multipart(&path).await.unwrap()));
     let total_start = std::time::Instant::now();
+
+    let mut tasks = Vec::with_capacity(total_size as usize / part_size as usize + 1);
     while bytes_written < total_size {
-        let start = std::time::Instant::now();
-        log::info!("About to upload {} bytes of data", data.len());
-        multipart
-            .put_part(PutPayload::from_bytes(data.clone()))
-            .await
-            .unwrap();
-        log::info!(
-            "Upload took {:?} seconds progress={}",
-            start.elapsed().as_secs_f64(),
-            bytes_written as f64 / total_size as f64
-        );
+        let data = data.clone();
+        let multipart = multipart.clone();
+        tasks.push(async move {
+            let start = std::time::Instant::now();
+            log::info!("About to upload {} bytes of data", data.len());
+            let mut multipart = multipart.lock().unwrap();
+            multipart
+                .put_part(PutPayload::from_bytes(data.clone()))
+                .await
+                .unwrap();
+            log::info!(
+                "Upload took {:?} seconds progress={}",
+                start.elapsed().as_secs_f64(),
+                bytes_written as f64 / total_size as f64
+            );
+        });
         bytes_written += part_size;
     }
+    futures::stream::iter(tasks)
+        .buffered(max_parallelism as usize)
+        .collect::<Vec<_>>()
+        .await;
+
+    let mut multipart = multipart.lock().unwrap();
     multipart.complete().await.unwrap();
     log::info!(
         "Total upload took {:?} seconds",
